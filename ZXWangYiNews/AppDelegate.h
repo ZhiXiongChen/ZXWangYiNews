0fa42bdca05a3ccfcdf6271063f74f8a78dc4fa2
ZhiXiongChen <784105464@qq.com>
2017-08-19 12:49:23 +0800

    Initial Commit

---
+++ ZXWangYiNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXWangYiNews
+//
+//  Created by zhixiongchen on 2017/8/19.
+//  Copyright © 2017年 zhixiongchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXWangYiNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXWangYiNews
+//
+//  Created by zhixiongchen on 2017/8/19.
+//  Copyright © 2017年 zhixiongchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
